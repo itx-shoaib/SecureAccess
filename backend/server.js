0001ba61c8001a359963137c864cd0fb09641af1
itx-shoaib <shoaibjamil43@gmail.com>
2023-07-04 00:26:15 +0500

    making a simple server

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const port = 5000;
+
+const app = express()
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port,()=>console.log(`Server is running on port ${port}`))
