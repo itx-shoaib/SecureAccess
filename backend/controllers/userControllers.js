2c7599fc9d326c9687d203371da64cc4aecb0e92
itx-shoaib <shoaibjamil43@gmail.com>
2023-07-05 01:48:52 +0500

    adding asyncHandler in userController

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,9 +1,11 @@
+import asyncHandler from "express-async-handler"
+
 //  @desc   :  Auth user /set token
 //  @Route  :  POST /api/users/auth
 //  @access :  Public
-const authUser = (req,res)=>{
+const authUser = asyncHandler( async (req,res)=>{
     res.status(200).json({message:"Auth Test"})
-}
+})
 
 //  Exporting the routes
 export {
