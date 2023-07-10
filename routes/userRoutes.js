d971cf060ef9508f05a84b475da7f430ed81a692
itx-shoaib <shoaibjamil43@gmail.com>
2023-07-11 02:07:31 +0500

    making authMiddleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,11 +9,12 @@ import {
     updateUserProfile,
     registerUser
 } from "../controllers/userControllers.js";
+import { protect } from "../middlewares/authMiddlewares.js";
 
 // making the routes
 router.post("/auth", authUser);
 router.post("/", registerUser);
 router.post("/logout", logoutUser);
-router.route("/profile").get(getUserProfile).put(updateUserProfile)
+router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile)
 
 export default router;
